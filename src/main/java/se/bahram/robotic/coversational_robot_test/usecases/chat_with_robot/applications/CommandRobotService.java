df8f08359c03f966cdcaaf97c12fb02afdfa51a1
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-12-27 19:39:29 +0100

    Command Robot

---
+++ src/main/java/se/bahram/robotic/coversational_robot_test/usecases/chat_with_robot/applications/CommandRobotService.java
@@ -0,0 +1,4 @@
+package se.bahram.robotic.coversational_robot_test.usecases.chat_with_robot.applications;
+
+public class CommandRobotService {
+}
---
+++ src/main/java/se/bahram/robotic/coversational_robot_test/utilities/AiAgentPersona.java
@@ -0,0 +1,4 @@
+package se.bahram.robotic.coversational_robot_test.utilities;
+
+public class AiAgentPersona {
+}
